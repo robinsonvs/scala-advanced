f86b73ab787596387d7b3a7f99b0ac9d8dda98e2
robinsonvs <robinsonvs@gmail.com>
2021-02-02 18:07:40 -0300

    add monads - exercises

--- src/lectures/part02AdvancedFunctionalProgramming/Monads.scala
+++ src/lectures/part02AdvancedFunctionalProgramming/Monads.scala
@@ -79,6 +79,58 @@ object Monads extends App {
 
           (have List in mind)
    */
-  
+
+  //1 - Lazy monad
+  class Lazy[+A](value: => A) {
+    //call by need
+    private lazy val internalValue = value
+    def use: A = value
+    def flatMap[B](f: (=> A) => Lazy[B]): Lazy[B] = f(internalValue)
+  }
+  object Lazy {
+    def apply[A](value: => A): Lazy[A] = new Lazy(value) //unit
+  }
+
+  val lazyInstance = Lazy {
+    println("Today I don't feel like doing anything")
+    42
+  }
+  //println(lazyInstance.use)
+
+  val flatMappedInstance = lazyInstance.flatMap(x => Lazy {
+    10 * x
+  })
+  val flatMappedInstance2 = lazyInstance.flatMap(x => Lazy {
+    10 * x
+  })
+  flatMappedInstance.use
+  flatMappedInstance2.use
+
+  /*
+    left-identity
+    unit.flatMap(f) = f(v)
+    Lazy(v).flatMap(f) = f(v)
+
+    right-identity
+    l.flatMap(unit) = l
+    Lazy(v).flatMap(x => Lazy(x)) = Lazy(v)
+
+    associativity: l.flatMap(f).flatMap(g) = l.flatMap(x => f(x).flatMap(g))
+
+    Lazy(v).flatMap(f).flatMap(g) = f(v).flatMap(g)
+    Lazy(v).flatMap(x => f(x).flatMap(g) = f(v).flatMap(g)
+   */
+
+  //2 - map and flatten in terms of flatMap
+  /*
+    Monad[T] { //List
+      def flatMap[B](f: T => Monad[B]): Monad[B] = ... (implemented)
+
+      def map[B](f: T => B): Monad[B] = flatMap(x => unit(f(x))) //Monad[B]
+      def flatten(m: Monad[Monad[T]]): Monad[T] = m.flatMap((x: Monad[T]) => x)
+
+      List(1,2,3).map(_ * 2) = List(1,2,3).flatMap(x => List(x * 2))
+      List(List(1,2), List(3,4)).flatten = List(List(1,2), (3,4)).flatMap(x => x) = List(1,2,3,4)
+   */
 
 }
